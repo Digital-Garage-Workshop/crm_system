379cc27f26df66f1f07ef4ce2ad2e5c2059913c7
temuujinnn <temvkaa@gmail.com>
2025-05-11 12:16:23 +0800

    notif

--- app/jobs/messages/contact_push_notification_job.rb
+++ app/jobs/messages/contact_push_notification_job.rb
@@ -3,10 +3,15 @@ module Messages
     queue_as :medium
 
     def perform(message_id)
-      @message = Message.find_by(id: message_id)
-      return if @message.blank?
+      message = Message.find_by(id: message_id)
 
-      Messages::ContactPushNotificationService.new(message: @message).perform
+      if message.nil?
+        Rails.logger.error "ContactPushNotificationJob: Message #{message_id} not found"
+        return
+      end
+
+      Rails.logger.info "ContactPushNotificationJob: Processing push notification for message #{message_id}"
+      Messages::ContactPushNotificationService.new(message: message).perform
     end
   end
 end
