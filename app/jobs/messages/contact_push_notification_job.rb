ef9426b8e0b95a4ca94617c1a6a1782a396ba941
temuujinnn <temvkaa@gmail.com>
2025-05-21 11:27:45 +0800

    bug fix

--- app/jobs/messages/contact_push_notification_job.rb
+++ app/jobs/messages/contact_push_notification_job.rb
@@ -3,9 +3,15 @@ module Messages
     queue_as :medium
 
     def perform(message_id)
-      @message = Message.find_by(id: message_id)
-      return if @message.blank?
+      Rails.logger.info "[PUSH-JOB] ContactPushNotificationJob эхэллээ. message_id=#{message_id}"
 
+      @message = Message.find_by(id: message_id)
+      if @message.blank?
+        Rails.logger.warn '[PUSH-JOB] @message хоосон байна. Push notification илгээхгүй.'
+        return
+      end
+
+      Rails.logger.info "[PUSH-JOB] @message олдлоо (id=#{@message.id}), push notification сервис рүү илгээж байна."
       Messages::ContactPushNotificationService.new(message: @message).perform
     end
   end
